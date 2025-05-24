9a0c12f7d58e17870352e643124055c54c8c56d8
Anton <walltouch@gmail.com>
2025-05-24 13:08:57 +0300

    Sprint 10 ImageFeed: OAuth2

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,41 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by ANTON ZVERKOV on 21.05.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "lHCB0PWcn162gTmatf6S0NgUzfm5B0M9rk2wLAlTqoQ"
+    static let secretKey = "N7rvuvBRH6LLroOzUK7He-vSm225xK6qkfxe0yAd5Zo"
+    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
+
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    static let tokenURLString = "https://unsplash.com/oauth/token"
+}
+
+enum KeyConstants {
+    static let accessTokenKey = "accessToken"
+}
+
+//TODO: To use later?:
+//extension Constants {
+//    enum Secrets {
+//        static let accessKey = Secrets.environmentVariable(named: "UNSPLASH_ACCESS_KEY") ?? ""
+//        static let secretKey = Secrets.environmentVariable(named: "UNSPLASH_SECRET_KEY") ?? ""
+//        
+//        private static func environmentVariable(named name: String) -> String? {
+//            guard let infoDictionary = Bundle.main.infoDictionary, let value = infoDictionary[name] as? String else {
+//                print("!! Missing Environment Variable: '\(name)'")
+//                return nil
+//            }
+//            return value
+//        }
+//    }
+//}
---
+++ ImageFeed/OAuth/OAuth2Service.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    private init() {}
+    
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard let tokenURL = URL(string: WebViewConstants.tokenURLString) else {
+            print("Could not create tokenURL")
+            return nil
+        }
+        var request = URLRequest(url: tokenURL)
+        request.httpMethod = "POST"
+        
+        let params = [
+            "client_id": Constants.accessKey,
+            "client_secret": Constants.secretKey,
+            "redirect_uri": Constants.redirectUri,
+            "code": code,
+            "grant_type": "authorization_code"
+        ]
+        
+        var components = URLComponents()
+        components.queryItems = params.compactMap { URLQueryItem(name: $0.key, value: $0.value) }
+        
+        request.httpBody = components.query?.data(using: .utf8)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        return request
+    }
+    
+    func fetchOAuthToken(code: String, handler: @escaping (Result<String, Error>) -> Void) {
+        guard let request = makeOAuthTokenRequest(code: code) else { return }
+        let task = URLSession.shared.data(for: request) { result in
+            switch result {
+            case .success(let tokenResponseBody):
+                do {
+                    let decoder = JSONDecoder()
+                    let token = try decoder.decode(OAuthTokenResponseBody.self, from: tokenResponseBody).accessToken
+                    handler(.success(token))
+                } catch {
+                    print("Failed to decode token from JSON")
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
